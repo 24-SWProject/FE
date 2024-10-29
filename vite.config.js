bdba0e1b919f4bbb5849220bfc7862ec3b05156f
9aeun <rlarkdms0424@naver.com>
2024-10-29 10:44:52 +0900

    vite 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true // 모든 경로를 index.html로 리디렉션
+  }
 })
