2dc0a8782960e70b9663b59de127afaec0584d9c
GaEun Kim <rlarkdms0424@naver.com>
2024-12-04 11:54:52 +0900

    그룹 여부 console

--- src/api/groupcrud.js
+++ src/api/groupcrud.js
@@ -27,6 +27,7 @@ export const joinGroup = async (code) => {
 export const checkGroupJoin = async () => {
     try {
         const response = await instance.get(`/api/auth/group/join/check`);
+        console.log("그룹 여부: ", response.data);
         return response; // `response.data`가 true/false라고 가정
     } catch (error) {
         console.error("그룹 참여 여부 확인 에러 : ", error.response?.data || error.message);
