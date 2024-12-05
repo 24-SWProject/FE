d879ca9bc7d76269680bd726c46c7097dce21d21
9aeun <rlarkdms0424@naver.com>
2024-12-05 10:20:18 +0900

    TodoModal overlay 스타일 수정

--- src/styles/components/TodoModal.style.js
+++ src/styles/components/TodoModal.style.js
@@ -5,7 +5,7 @@ export const ModalOverlay = styled.div`
     top: 0;
     left: 0;
     width: 100%;
-    height: 100vh;
+    height: 100%;
     background-color: rgba(0, 0, 0, 0.5); /* 어두운 반투명 배경 */
     display: flex;
     justify-content: center;
